52ebfa2902b0246afe7c25cebc482bdac722fcdb
priddhesh <109747774+priddhesh@users.noreply.github.com>
2023-06-05 09:50:00 +0530

    Create Next Greater Element.cpp

---
+++ Next Greater Element.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+
+vector<int> nextGreater(vector<int> &arr, int n) {
+    stack<int>st;
+    vector<int>ans;
+    for(int i=n-1;i>=0;i--)
+    {
+        if(st.empty())
+        {
+            ans.push_back(-1);
+        }
+        else{
+            while(!st.empty() && st.top()<=arr[i])
+            {
+                st.pop();
+            }
+            if(st.empty())
+            {
+               ans.push_back(-1);
+            }else{
+               ans.push_back(st.top());
+            }
+        }
+        st.push(arr[i]);
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
